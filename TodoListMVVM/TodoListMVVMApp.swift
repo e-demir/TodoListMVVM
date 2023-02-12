40979752b96c1add5be42a355f72adb74d90cd53
dev_demir <dev_demir@outlook.com>
2023-02-12 14:47:57 +0300

    Initial Commit

---
+++ TodoListMVVM/TodoListMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListMVVMApp.swift
+//  TodoListMVVM
+//
+//  Created by Emrullah Demir on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
