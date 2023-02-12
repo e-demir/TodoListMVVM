cfe3fbc75b0bf4b04044aca67277254b80413212
dev_demir <dev_demir@outlook.com>
2023-02-12 15:01:11 +0300

    ListRowItem title made dynamic

--- TodoListMVVM/Views/ListRowView.swift
+++ TodoListMVVM/Views/ListRowView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct ListRowView: View {
+    let title: String
+    
     var body: some View {
         HStack{
             Image(systemName: "checkmark.circle")
-            Text("First item of list")
+            Text(title)
         }
     }
 }
 struct ListRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ListRowView()
+        ListRowView(title: "First item of the list")
     }
 }
--- TodoListMVVM/Views/ListView.swift
+++ TodoListMVVM/Views/ListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ListView: View {
     var body: some View {
         List {
-            ListRowView()
+            ListRowView("This is the first item ")
         }.navigationTitle("ToDo 📝")
     }
 }
