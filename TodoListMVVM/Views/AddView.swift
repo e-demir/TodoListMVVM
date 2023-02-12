b31abdbff82bc0b29705ae3d97c626a6619ac918
dev_demir <dev_demir@outlook.com>
2023-02-12 15:17:58 +0300

    AddView development

--- TodoListMVVM/Views/AddView.swift
+++ TodoListMVVM/Views/AddView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct AddView: View {
+    @State var note: String = ""
+    
     var body: some View {
-        Text("Add View")
+        ScrollView {
+            VStack {
+                TextField("Your note...", text: $note)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color.gray.brightness(0.4))
+                    .cornerRadius(15)
+                Button {
+                    
+                } label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline )
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(15)
+                }
+
+            }.padding(14)
+                
+        }
+        .navigationTitle("Add note 🖊")
     }
 }
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        AddView()
+        NavigationView {
+            AddView()
+        }
     }
 }
